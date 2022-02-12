6569130b8f9dbb741b384e630dadd0ec5b02860f
haniwaman <haniwa008@gmail.com>
2022-02-12 23:34:17 +0900

    seriesとparallelの確認

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
 const gulp = require('gulp'); // gulpの関数を使えるように
 
 // 1、タスクの作成
-function copyFile() {
-  return gulp.src('./src/demo.js') // 元のファイル
-    .pipe(gulp.dest('./public/')) // 出力先のファイル
+function taskA(done) {
+  console.log('taskA');
+  done();
 }
 
 // 1、タスクの作成
-function fileWatch() {
-  gulp.watch('src/demo.js', copyFile); // ファイルの監視
-};
+function taskB(done) {
+  console.log('taskB');
+  done();
+}
+
+// 1、タスクの作成
+function taskC(done) {
+  console.log('taskC');
+  done();
+}
 
 // 2、タスクの登録
-exports.copyFile = copyFile // 3、タスクの実行
-exports.fileWatch = fileWatch // 3、タスクの実行
+exports.seriesDemo = gulp.series(taskA, taskB, taskC); // 3、タスクの実行
+exports.parallelDemo = gulp.parallel(taskA, taskB, taskC); // 3、タスクの実行
