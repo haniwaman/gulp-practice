fcaaade1be112ea16eca5033b424e36ba043fb8a
haniwaman <haniwa008@gmail.com>
2022-02-13 10:50:48 +0900

    autoprefixerの追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const gulp = require('gulp'); // gulpの関数を使えるように
 const sass = require("gulp-sass")(require("sass")); // gulp-sassの読み込み（https://www.npmjs.com/package/gulp-sass）
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer"); // （https://github.com/postcss/autoprefixer）
 
 // 1、タスクの作成
 function compileSass() {
 	return gulp.src("./src/assets/sass/**/*.scss")
   // srcとdestの間に変換用の処理を書く
 	.pipe(sass())
+  .pipe(postcss([autoprefixer()]))
 	.pipe(gulp.dest("./public/assets/css/"))
 }
 
