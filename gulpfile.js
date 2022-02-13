da5015dcc4132fe69ce9daa80787d2de15a228bb
haniwaman <haniwa008@gmail.com>
2022-02-13 11:20:25 +0900

    変更があったらリロード

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,23 @@ const browserSync = require("browser-sync"); //（https://browsersync.io/docs/gu
 function browserInit(done) {
 	browserSync.init({
 		server: {
-			baseDir: "./public/" // 表示対象のディレクトリを指定
+			baseDir: './public/' // 表示対象のディレクトリを指定
 		}
 	});
   done();
 }
 
+// ブラウザのリロード
+function browserReload(done) {
+	browserSync.reload();
+	done();
+}
+
+// ファイル監視
+function watch() {
+  // sassファイルが更新されたらブラウザをリロード
+	gulp.watch("./src/assets/sass/**/*.scss", browserReload);
+}
+
 // 2、タスクの登録
-exports.browserInit = browserInit; // 3、タスクの実行
+exports.watch = gulp.series(browserInit, watch); // 3、タスクの実行（browserInit実行の後、ファイル監視を実行）
