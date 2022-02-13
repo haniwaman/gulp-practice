00bf22e8f61410336044abff559fcd9be9af9672
haniwaman <haniwa008@gmail.com>
2022-02-13 10:31:10 +0900

    gulp-sassのdemo

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,13 @@
 const gulp = require('gulp'); // gulpの関数を使えるように
+const sass = require("gulp-sass")(require("sass")); // gulp-sassの読み込み（https://www.npmjs.com/package/gulp-sass）
 
 // 1、タスクの作成
-function taskA(done) {
-  console.log('taskA');
-  done();
-}
-
-// 1、タスクの作成
-function taskB(done) {
-  console.log('taskB');
-  done();
-}
-
-// 1、タスクの作成
-function taskC(done) {
-  console.log('taskC');
-  done();
+function compileSass() {
+	return gulp.src("./src/assets/sass/**/*.scss")
+  // srcとdestの間に変換用の処理を書く
+	.pipe(sass())
+	.pipe(gulp.dest("./public/assets/css/"))
 }
 
 // 2、タスクの登録
-exports.seriesDemo = gulp.series(taskA, taskB, taskC); // 3、タスクの実行
-exports.parallelDemo = gulp.parallel(taskA, taskB, taskC); // 3、タスクの実行
+exports.compileSass = compileSass; // 3、タスクの実行
--- src/demo.js
+++
@@ -1,1 +0,0 @@
-console.log('Gulpでファイルの変換（出力）');
