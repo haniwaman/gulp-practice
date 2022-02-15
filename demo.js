7dd71705bb015caa3b30b51f218b873de85a89f9
haniwaman <haniwa008@gmail.com>
2022-02-15 20:10:12 +0900

    node.jsのrequireとexportsの関係イメージ

---
+++ demo.js
@@ -0,0 +1,7 @@
+exports.getName = getName = () => {
+  return 'Tom';
+};
+
+exports.getAge = getAge = () => {
+  return 30;
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp'); // gulpの関数を使えるように
 const sass = require("gulp-sass")(require("sass"));
 const browserSync = require("browser-sync"); //（https://browsersync.io/docs/gulp）
+const person = require('./demo');
+
+function nodeDemo(done) {
+  console.log("Name", person.name());
+  console.log("Age", person.age());
+  done();
+}
 
 function compileSass() {
 	return gulp.src("./src/assets/sass/**/*.scss")
@@ -32,3 +39,4 @@ function watch() {
 
 // 2、タスクの登録
 exports.watch = gulp.series(browserInit, watch); // 3、タスクの実行（browserInit実行の後、ファイル監視を実行）
+exports.nodeDemo = nodeDemo; // 3、タスクの実行（browserInit実行の後、ファイル監視を実行）
